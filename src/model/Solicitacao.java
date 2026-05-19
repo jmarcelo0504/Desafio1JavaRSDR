cfc986cdb681c6e245109c3a4284765b505c55f7
jmarcelo0504 <marcelinhogregorio44@gmail.com>
2026-05-19 17:57:15 -0300

    Criação do modelo da solicitação

--- src/model/Solicitacao.java
+++ src/model/Solicitacao.java
@@ -1,3 +1,26 @@
+package model;
+
 public class Solicitacao {
 
+    private int id;
+    private Beneficiario beneficiario;
+    private ItemDoacao item;
+    private int quantidadeSolicitada;
+    private String justificativa;
+    private String status;
+
+    public Solicitacao(int id, Beneficiario beneficiario, ItemDoacao item, int quantidadeSolicitada, String justificativa, String status) {
+
+        this.id = id;
+        this.beneficiario = beneficiario;
+        this.item = item;
+        this.quantidadeSolicitada = quantidadeSolicitada;
+        this.justificativa = justificativa;
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "Solicitação Nº" + id + ", Beneficiário: " + beneficiario.getNome() + ", Item: " + item.getNome() + ", Quantidade: " + quantidadeSolicitada + ", Status: " + status;
+    }
 }
